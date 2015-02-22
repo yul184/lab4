9cda46ba743301dd702304493396eb06db63fafd
vagrant <vagrant@precise32.(none)>
2015-02-22 03:47:34 +0000

    node.js lab

--- routes/project.js
+++ routes/project.js
@@ -2,5 +2,7 @@
 exports.viewProject = function(req, res) {
 	var name = req.params.name; 
 	console.log("The project name is: " + name);
-	res.render('project');
+	res.render('project', {
+		'projectName': name
+	});
 }
